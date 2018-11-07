41e461764b8200715ad5e311f31a209dcc0feb43
Jddanan <jddanan@gmail.com>
2018-11-07 20:11:28 +0200

    Created function draw and event listener

--- js/Paint.js
+++ js/Paint.js
@@ -1,6 +1,21 @@
-function showCoords(event) {
-    var x = event.clientX;
-    var y = event.clientY;
-    var coor = "X coords: " + x + ", Y coords: " + y;
-    document.getElementById("coord").innerHTML = coor;
+var canvas = document.getElementById("canvas")
+var newDiv;
+function draw() {
+    newDiv = document.createElement("div");
+    canvas.appendChild(newDiv);
+    newDiv.style.backgroundColor = "black";
+    newDiv.style.width = "4px";
+    newDiv.style.height = "4px";
+    newDiv.style.borderRadius = "50%";
+    newDiv.style.position = "absolute";
+    newDiv.style.left = event.pageX + "px";
+    newDiv.style.top = event.pageY + "px";
+
 }
+canvas.addEventListener("mousedown", function () {
+    canvas.addEventListener("mousemove", draw)
+});
+
+canvas.addEventListener("click",function(){
+    canvas.removeEventListener("mousemove",draw)
+})
